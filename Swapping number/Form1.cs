d33c578237354973f449ab41643830d4477388b1
Chanchal <mr.ckchiranjivi07@gmail.com>
2024-09-22 00:49:39 +0530

    Add project files.

---
+++ Swapping number/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swapping_number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            int a, b;
+            a=Convert.ToInt32(textBox1.Text);
+            b=Convert.ToInt32(textBox2.Text);
+            a = a + b;
+            b=a-b; 
+            a = a-b;
+            textBox3.Text=a.ToString();
+            textBox4.Text=b.ToString();
+        }
+    }
+}
